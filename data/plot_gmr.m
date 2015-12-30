cb4e0e8930f5db65f498550f813c135764b7b8fc
Thomas Eiband <thomas.eiband@gmail.com>
2015-12-30 19:02:45 +0100

    descriptions for plots

--- data/plot_gmr.m
+++ data/plot_gmr.m
@@ -6,15 +6,20 @@ load xtraj.mat
 load straj.mat
 load vtraj.mat
 
-
-plot(xtraj(1,:), xtraj(2,:),'*')
 axis equal
+plot(xtraj(1,:), xtraj(2,:),'*')
+xlabel('x'); ylabel('y');
+title('Position')
+%xlim([-0.5, 0.5]); ylim([-0.5, 0.5])
 
 figure
 xx = 1:length(vtraj);
 plot(xx, vtraj(1,:),xx, vtraj(2,:),xx, vtraj(3,:))
-title vtraj
+title('Velocity')
+legend('x', 'y', 'theta');
+xlabel('timestep'); ylabel('m/s or rad/s')
 
 figure
 plot(xx, straj(1,:),xx, straj(2,:),xx, straj(3,:))
-title straj
+title('Clock Signal s')
+xlabel('timestep'); ylabel('s')
